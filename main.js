36b902477eb0aca412c82f6ece50329c5e7e6e39
weikh <algoritm1202@gmail.com>
2022-10-31 09:06:18 +0500

    salom

---
+++ main.js
@@ -0,0 +1,99 @@
+// N1  1000 gacha toq sonlar chiqarish;
+// function toq (son) {
+//     for (i = 1; i <= son; i++) {
+//         if (i % 2 == 1) {
+//             console.log(i);
+//         }
+//     } 
+// }
+// toq (1000);
+
+
+// // N2 1000 gacha juft sonlar chiqarish;
+// function juft (son) {
+//     for (i = 1; i <= son; i++) {
+//         if (i % 2 == 0) {
+//             console.log(i);
+//         }
+//     } 
+  
+// }
+// juft (1000)
+
+
+// // N3 1000 gacha toq sonlar yig'indisi chiqarish;
+// let sum = 0;
+// function toqSum(son) {
+//     for (i = 1; i <= son; i++) {
+//         if (i % 2 == 1) {
+//             sum = sum + i;
+//         }
+//     }
+//     console.log(sum);
+// }
+// toqSum(1000)
+
+
+// N4 1000 gacha juft sonlar yig'indisi;
+// let sum = 0;
+// function juftSum(son) {
+//     for (i = 1; i <= son; i++) {
+//         if (i % 2 == 0) {
+//             sum = sum + i;
+//         }
+//     }
+//     console.log(sum);
+// }
+// juftSum(1000)
+
+
+// //N5 1000 gacha 3 bo'linadigan sonlar chiqarish;
+// function uch(num) {
+//     for (i = 1; i <= num; i++) {
+//         if (i % 3 == 0) {
+//             console.log(i);
+//         }
+//     }
+// }
+// uch(1000);
+
+
+// N6 2 ta sonni funksiya orqali ayirish;
+// let num1 = Number(prompt("birinchi sonni kiriting"));
+// let num2 = Number(prompt("ikkinchi sonni kiriting"));
+// function ayirish (num1, num2) {
+//     return num1 - num2;
+// }
+// let natija = ayirish(num1, num2);
+// console.log(natija);
+
+
+// // N7 2 ta sonni funksiya orqali bo'lish;
+// let num1 = Number(prompt("birinchi sonni kiriting"));
+// let num2 = Number(prompt("ikkinchi sonni kiriting"));
+// function bulish(num1, num2) {
+//     return num1 / num2;
+// }
+// natija = bulish(num1,num2);
+// console.log(natija);
+
+
+
+// // N8 2 ta sonni funksiya orqali ko'paytirish;
+// let num1 = Number(prompt("birinchi sonni kiriting"))
+// let num2 = Number(prompt("ikkinchi sonni kiriting"))
+// function kupaytirish(num1, num2) {
+//     return num1 * num2;
+// }
+// let natija = kupaytirish(num1, num2);
+// console.log(natija); 
+
+
+// N9 sonning darajasini topish funksiyasi;
+// let son = Number(prompt("sonni kiriting"));
+// let daraja = Number(prompt("darajani kiriting"));
+// function amal(son, daraja) {
+//     return son ** daraja;
+// }
+// let natija = amal(son,daraja);
+// console.log(natija);
